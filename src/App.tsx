4e5e1b62a97a1ee0946f7e7eb1053148ce25aca9
Sclingan <sclingan1@gmail.com>
2024-06-12 12:52:57 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './header'
+import Article from './article'
+import Footer from './footer'
+
+
+function App() {
+
+  return (
+    <main>
+      <Header />
+      <Article />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/header.tsx
@@ -0,0 +1,13 @@
+
+
+function Header() {
+
+    return (
+        <header>
+            
+        </header>
+   
+    )
+  }
+  
+  export default Header
