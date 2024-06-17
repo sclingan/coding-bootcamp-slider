d3cbc9d0c0ceed3003e83df3ed975fb381cbe0c2
Sclingan <sclingan1@gmail.com>
2024-06-17 13:00:20 -0500

    Adding prev and next controls

--- src/article.tsx
+++ src/article.tsx
@@ -4,19 +4,19 @@ function Article() {
 
     return (
         <article>
-            <p>“ I’ve been interested in coding for a while but never taken the jump, until now. 
+            <p className="profile_p" id='profile_1'>“ I’ve been interested in coding for a while but never taken the jump, until now. 
                  I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
                  excited about the future. ”</p>
 
-                <span className="name">Tanya Sinclair</span>
-                <p className="title">UX Engineer</p>
+                <span className="name" id='profile_1_name'>Tanya Sinclair</span>
+                <p className="title" id='profile_1_title'>UX Engineer</p>
 
-  {/* “ If you want to lay the best foundation possible I’d recommend taking this course. 
-  The depth the instructors go into is incredible. I now feel so confident about 
-  starting up as a professional developer. ”
+            <p className="visually-hidden" id='profile_2'>“ If you want to lay the best foundation possible I’d recommend taking this course. 
+                The depth the instructors go into is incredible. I now feel so confident about 
+                starting up as a professional developer. ”</p>
 
-  John Tarkpor
-  Junior Front-end Developer */}
+                <span className="visually-hidden" id='profile_2_name'>John Tarkpor</span>
+                <p className="visually-hidden" id='profile_2_title'>Junior Front-end Developer</p>
         </article>
    
     )
--- src/header.tsx
+++ src/header.tsx
@@ -1,15 +1,56 @@
 import tanya from '/images/image-tanya.jpg';
+import john from '/images/image-john.jpg';
 import next from '/images/icon-next.svg';
 import prev from '/images/icon-prev.svg';
 
 function Header() {
+    
+    function slider() {
+        const user1 = document.getElementById('profile_1_img');
+        const user2 = document.getElementById('profile_2_img');
+        const user1_profile = document.getElementById('profile_1');
+        const user2_profile = document.getElementById('profile_2');
+        const user1_name = document.getElementById('profile_1_name');
+        const user1_title = document.getElementById('profile_1_title');
+        const user2_name = document.getElementById('profile_2_name');
+        const user2_title = document.getElementById('profile_2_title');
+
+        let currentProfile = document.getElementById('profile_1_img');
+        let nextProfile = document.getElementById('profile_2_img');
+
+
+    if(user1?.className === 'profile'){
+        user1?.classList.remove('profile');
+        user1?.classList.add('visually-hidden');
+        user1_profile?.classList.remove('profile_p');
+        user1_profile?.classList.add('visually-hidden');
+        user1_name?.classList.remove('name');
+        user1_name?.classList.add('visually-hidden');
+        user1_title?.classList.remove('title');
+        user1_title?.classList.add('visually-hidden');
+        user2?.classList.remove('visually-hidden');
+        user2?.classList.add('profile');
+        user2_profile?.classList.remove('visually-hidden');
+        user2_profile?.classList.add('profile_p');
+        user2_name?.classList.remove('visually-hidden');
+        user2_name?.classList.add('name');
+        user2_title?.classList.remove('visually-hidden');
+        user2_title?.classList.add('title');
+    }else{
+        user1?.classList.remove('visually-hidden');
+        user1?.classList.add('profile');
+        user2?.classList.remove('profile');
+        user2?.classList.add('visually-hidden');
+    }
+}
 
     return (
         <header>
-            <img src={tanya} className='profile' alt=""></img>
+            <img src={tanya} className='profile' id='profile_1_img' alt=""></img>
+            <img src={john} className='visually-hidden' id='profile_2_img' alt=""></img>
             <div className='slider'>
-                <img src={prev} alt='back arrow button'></img>
-                <img src={next} alt='next arrow button'></img>
+                <img src={prev} onClick={slider} alt='back arrow button'></img>
+                <img src={next} onClick={slider} alt='next arrow button'></img>
             </div>
         </header>
     )
